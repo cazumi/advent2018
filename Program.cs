8fef8b2884751c5effd351e7f29604ec6b372689
Cazumi <cazumi1989@gmail.com>
2018-12-05 10:20:01 +0100

    initial setup

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace advent2018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!2");
+        }
+    }
+}
